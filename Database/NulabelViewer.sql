6f5f319a129e7fd9380a4c44d732a6fe576f4d01
Nguyen Dat <datnguyen1abc@gmail.com>
2026-03-14 15:30:27 +0700

    figma & database

---
+++ Database/NulabelViewer.sql
@@ -0,0 +1,22 @@
+﻿-- Bảng 1: Lưu thông tin 6 ảnh camera (View)
+CREATE TABLE Scenes (
+    SceneID INT PRIMARY KEY IDENTITY(1,1),
+    SampleToken VARCHAR(100) UNIQUE,
+    ImgPath_Front NVARCHAR(255),
+    ImgPath_Back NVARCHAR(255),
+    ImgPath_FrontLeft NVARCHAR(255),
+    ImgPath_FrontRight NVARCHAR(255),
+    ImgPath_BackLeft NVARCHAR(255),
+    ImgPath_BackRight NVARCHAR(255)
+);
+
+-- Bảng 2: Lưu nhãn và trạng thái CRUD
+CREATE TABLE Annotations (
+    AnnoID INT PRIMARY KEY IDENTITY(1,1),
+    SampleToken VARCHAR(100),
+    Category NVARCHAR(50),
+    X_Coord FLOAT, Y_Coord FLOAT, Width FLOAT, Height FLOAT,
+    ReviewStatus INT DEFAULT 0, -- 0: Chưa xem, 1: Đúng, 2: Sai
+    Note NVARCHAR(MAX),
+    CONSTRAINT FK_Scene FOREIGN KEY (SampleToken) REFERENCES Scenes(SampleToken)
+);
